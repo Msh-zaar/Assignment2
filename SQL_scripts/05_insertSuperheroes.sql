1c7b8e245f0a6abf3e4d511f6281870e59be3591
Michael Zaar <60154804+Msh-zaar@users.noreply.github.com>
2022-01-23 18:11:38 +0100

    added 4-6

---
+++ SQL_scripts/05_insertSuperheroes.sql
@@ -0,0 +1,9 @@
+USE [SuperheroesDb]
+GO
+
+INSERT INTO [dbo].[Superhero] ([Name], [Alias], [Origin])
+     
+VALUES ('Batman', 'Bruce Wayne', 'Gotham'),
+('Superman', 'Clark Kent', 'Krypton'),
+('Spiderman', 'Peter Parker', 'New York')
+GO
---
+++ SQL_scripts/06_insertAssistants.sql
@@ -0,0 +1,13 @@
+USE [SuperheroesDb]
+GO
+
+INSERT INTO [dbo].[Assistant]
+           ([Name]
+           ,[SuperheroId])
+     VALUES
+           ('Robin', 1),
+		   ('John Doe', 1),
+		   ('Louis Lane', 2)
+GO
+
+
